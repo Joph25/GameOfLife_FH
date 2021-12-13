074449cbe7642e609200a7fbaebfb0c8458d165d
Philipp Jonas <philipp.jonas25@gmail.com>
2021-12-13 16:37:19 +0100

    Began to wrote input parsers

--- GOL_sequential/board.cpp
+++ GOL_sequential/board.cpp
@@ -1,14 +1,50 @@
 #include "board.h"
 #include <iostream>
 #include <vector>
+#include <fstream>
+#include <string>
+using namespace std;
 
-void board::read_board_from_file(const char* file) {
+void board::read_board_from_file(const char* board_file) {
+	std::cout << board_file << std::endl;
+	std::ifstream infile(board_file);
+	if (!infile.is_open()) {
+		cout << "Cant open file " << board_file << endl;
+		return;
+	};
+	std::string line;
+	getline(infile, line);
+	string generations_str,size_str;
+	bool comma_flg = false;
+	for (auto it = line.cbegin(); it != line.cend(); ++it) {
+		if (*it == ',') {
+			comma_flg = true;
+			continue;
+		};
+		if (comma_flg) {
+			size_str.push_back(*it);
+		}
+		if (!comma_flg) {
+			generations_str.push_back(*it);
+		};
+		
 
+
+	}
+	cout << generations_str << endl;
+	cout << size_str << endl;
+	int generations = atoi(generations_str.c_str());
+	cout << generations << endl;
+
+	while (getline(infile, line))
+	{
+		//std::cout << line << std::endl;
+	};
 };
 
 void board::read_params_from_cmdline() {
-	std::cout << "read" << std::endl;
-	const char* testfile = "gol_examples\random250_in.gol";
+	const char* testfile = "..\\gol_examples\\random250_in.gol";
+	//const char* testfile = "C:\\Root_Philipp\\FH_AI\Visual_Studio_Projekte\\GameOfLife\\gol_examples\\random250_in.gol";
 	read_board_from_file(testfile);
 
 };
