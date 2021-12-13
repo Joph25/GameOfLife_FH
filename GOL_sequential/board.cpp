db059a00af20dff81f956ff0a8977613b356e2d1
Philipp Jonas <philipp.jonas25@gmail.com>
2021-12-13 18:26:53 +0100

    GOL Rules implemented

--- GOL_sequential/board.cpp
+++ GOL_sequential/board.cpp
@@ -21,6 +21,10 @@ void board::read_board_from_file(const char* board_file) {
 	for (int i = 0;i < size_y;++i) {
 		this->gol_board[i] = new bool[this->size_x];
 	};
+	this->next_gol_board = new bool* [this->size_y];
+	for (int i = 0;i < size_y;++i) {
+		this->next_gol_board[i] = new bool[this->size_x];
+	};
 	int line_counter = 0;
 	while (getline(infile, line))
 	{
@@ -48,7 +52,7 @@ void board::read_board_from_file(const char* board_file) {
 		};
 		line_counter++;
 	};
-	this->print_board_binary();
+	//this->print_board_binary();
 };
 
 void board::print_board_binary() {
@@ -66,9 +70,65 @@ void board::read_params_from_cmdline() {
 	read_board_from_file(testfile);
 
 };
+
 void board::compute_GOL() {
 	std::cout << "compute" << std::endl;
+	for (int gen = 0; gen < this->generations;gen++) {
+		this->update_board();
+	};
 };
+
+void board::update_board() {
+	for (int row = 0;row < this->size_y;++row) {
+		for (int col = 0;col < this->size_x;++col) {
+			this->next_gol_board[row][col] = this->update_cell(row, col);
+		};
+	};
+
+	//board switching
+	bool** temp_board = this->gol_board;
+	for (int i = 0;i < this->size_y;++i) {
+		temp_board[i] = this->gol_board[i];
+	};
+	this->gol_board = this->next_gol_board;
+	for (int i = 0;i < this->size_y;++i) {
+		this->gol_board[i] = this->next_gol_board[i];
+	};
+	this->next_gol_board = temp_board;
+	for (int i = 0;i < this->size_y;++i) {
+		this->next_gol_board[i] = temp_board[i];
+	};
+	
+};
+
+bool board::update_cell(int row, int col) {
+	//cout << row << "," << col << endl;
+	int dead_counter = 0;
+	int live_counter = 0;
+	bool cell_status = this->gol_board[row][col];
+	for (int y = row - 1; y < row + 1;++y) {
+		for (int x = col - 1; x < col + 1;++x) {
+			if ((x == row) && (y == col)) {
+				continue;
+			}
+			else if (this->gol_board[y % this->size_y][x % this->size_x]) { //if true -> alive
+				live_counter++;
+			}
+			else {
+				dead_counter++;
+			}
+		};
+	};
+	//Rules
+	if (!cell_status && (live_counter == 3)) return true;
+	if (cell_status && ((live_counter == 2) || (live_counter == 3))) return true;
+	if (cell_status && (live_counter < 2)) return false;
+	if (cell_status && (live_counter > 3)) return false;
+
+
+};
+
+
 void board::write_to_file() {
 	std::cout << "write" << std::endl;
 };
--- GOL_sequential/board.h
+++ GOL_sequential/board.h
@@ -8,7 +8,10 @@ private:
 	int size_y;
 	int generations;
 	bool **gol_board;
+	bool **next_gol_board;
 	void read_board_from_file(const char*);
+	void update_board();
+	bool update_cell(int,int);
 public:
 	void read_params_from_cmdline();
 	void compute_GOL();
